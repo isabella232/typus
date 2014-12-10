ab7ad4af69ce5999c09e51d3a6cff810f1738d08
Jakob Skjerning <jakob@mentalized.net>
2014-12-10 11:41:26 +0100

    Prevent options coming from trees from being HTML escaped

--- app/helpers/admin/resources/data_types/belongs_to_helper.rb
+++ app/helpers/admin/resources/data_types/belongs_to_helper.rb
@@ -13,7 +13,7 @@ module Admin::Resources::DataTypes::BelongsToHelper
     label_text = @resource.human_attribute_name(attribute)
 
     values = if related.respond_to?(:roots)
-      expand_tree_into_select_field(related.roots, related_fk)
+      raw(expand_tree_into_select_field(related.roots, related_fk))
     else
       related.order(related.typus_order_by).map { |p| [p.to_label, p.id] }
     end
